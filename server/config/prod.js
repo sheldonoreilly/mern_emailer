adf00e2d97e7113361fa753df0d7743d6679feee
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-21 19:20:02 -0230

    api keys for stripe

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,6 +5,8 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	//cookie key is for seeding the cookieSession (used fro encyrption)
-	cookieKey: process.env.COOKIE_KEY
+	//cookie key is for seeding the cookieSession (used for encyrption)
+	cookieKey: process.env.COOKIE_KEY,
+	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
