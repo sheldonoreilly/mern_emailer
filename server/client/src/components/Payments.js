1f402728456b584fec19fafbdc85adaafe34b35b
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-23 21:10:26 -0230

    Added purchase credit el

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import Typography from "@material-ui/core/Typography";
+
+const text = {
+	display: "inline",
+	marginRight: 10
+};
 
 class Payments extends Component {
 	render() {
 		return (
-			<StripeCheckout
-				amount={500}
-				token={token => console.log(token)}
-				stripeKey={process.env.REACT_APP_STRIPE_KEY}
-			/>
+			<Fragment>
+				<Typography style={text} color="inherit" variant="subheadingcaption" gutterBottom align="center">
+					Purchase Credits
+				</Typography>
+				<StripeCheckout
+					name={"Emailer"}
+					description={"$5 for 5 survey credits"}
+					amount={500}
+					token={token => console.log(token)}
+					stripeKey={process.env.REACT_APP_STRIPE_KEY}
+				>
+					<Button variant="fab" mini color="secondary">
+						<AddIcon />
+					</Button>
+				</StripeCheckout>
+			</Fragment>
 		);
 	}
 }
